b5d2460d5f8fb46076ebf0e1a1d36c349b38b6e9
Alexander Melnikov <okman5757@gmail.com>
2021-12-15 13:37:38 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import {Logger} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('BOOTSTRAP');
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 5000
+
+    await app.listen(PORT, () => logger.log(`Server started on ${PORT} port...`));
+  } catch (e) {
+    logger.error(e);
+    process.exit();
+  }
 }
 bootstrap();
