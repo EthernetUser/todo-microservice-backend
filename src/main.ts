fa412928a6fd9e63783a8503e96ee68b98976f70
Alexander Melnikov <okman5757@gmail.com>
2021-12-15 13:40:49 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import {Logger} from "@nestjs/common";
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('BOOTSTRAP');
   try {
     const app = await NestFactory.create(AppModule);
-    const PORT = process.env.PORT || 5000
+    const PORT = process.env.PORT || 5000;
 
-    await app.listen(PORT, () => logger.log(`Server started on ${PORT} port...`));
+    await app.listen(PORT, () =>
+      logger.log(`Server started on ${PORT} port...`),
+    );
   } catch (e) {
     logger.error(e);
     process.exit();
