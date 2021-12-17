a1a6f102e1b4e61d814b1860ae6bd0f2d28967c6
Alexander Melnikov <okman5757@gmail.com>
2021-12-18 01:04:06 +0300

    update login method and registration method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,20 +1,40 @@
-import {Body, Controller, Logger, Post, UsePipes, ValidationPipe} from '@nestjs/common';
-import {UserRegistrationDto} from "../dto/UserRegistration.dto";
-import {AuthService} from "./auth.service";
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { UserRegistrationDto } from '../dto/UserRegistration.dto';
+import { AuthService } from './auth.service';
+import { AppResponse } from '../helpers/AppResponse.helper';
+import { UserLoginDto } from '../dto/UserLogin.dto';
 
 @Controller('auth')
 export class AuthController {
-  private logger = new Logger('AUTH');
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login() {
-    return;
+  async login(@Body() dto: UserLoginDto) {
+    const token = await this.authService.authorizeUser(dto);
+    if (token) {
+      return new AppResponse<string>({
+        data: token,
+      });
+    } else {
+      return new AppResponse<null>({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: true,
+      });
+    }
   }
 
   @Post('registration')
-  @UsePipes(new ValidationPipe())
   async registration(@Body() dto: UserRegistrationDto) {
-    return this.authService.createUser(dto)
+    const user = await this.authService.createUser(dto);
+    if (user) {
+      return new AppResponse<null>({
+        statusCode: HttpStatus.CREATED,
+      });
+    } else {
+      return new AppResponse<null>({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: true,
+      });
+    }
   }
 }
