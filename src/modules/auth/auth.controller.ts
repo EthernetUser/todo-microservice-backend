c01b8bef9ab8d39e329de62646c66aacecefa494
Alexander Melnikov <okman5757@gmail.com>
2021-12-17 21:10:56 +0300

    update registration route

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import {Body, Controller, Logger, Post, UsePipes, ValidationPipe} from '@nestjs/common';
+import {UserRegistrationDto} from "../dto/UserRegistration.dto";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
   private logger = new Logger('AUTH');
+  constructor(private authService: AuthService) {}
 
   @Post('login')
   async login() {
@@ -10,7 +13,8 @@ export class AuthController {
   }
 
   @Post('registration')
-  async registration() {
-    return;
+  @UsePipes(new ValidationPipe())
+  async registration(@Body() dto: UserRegistrationDto) {
+    return this.authService.createUser(dto)
   }
 }
