41c18959641fd609f60175fcdcd24d57f7bf26c3
Alexander Melnikov <okman5757@gmail.com>
2021-12-17 20:23:02 +0300

    add createUser method in AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '../../schemas/user.schema';
+import { Model } from 'mongoose';
+import { UserRegistrationDto } from '../dto/UserRegistration.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private logger = new Logger('AUTHSERVICE');
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(dto: UserRegistrationDto) {
+    if (await this.validateNewUser(dto.email)) {
+      const user = await this.userModel.create(dto);
+      this.logger.log(
+        `Created new user: name - ${user.username}, email - ${user.email}.`,
+      );
+      return { statusCode: HttpStatus.CREATED };
+    } else {
+      this.logger.error(`Unsuccessful creating user.`);
+      return { statusCode: HttpStatus.BAD_REQUEST };
+    }
+  }
+
+  async validateNewUser(email: string) {
+    const user = await this.userModel.findOne({ email });
+    return !user;
+  }
+}
