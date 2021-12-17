456e8de92dbd580afa3d381e782540501a6396ab
Alexander Melnikov <okman5757@gmail.com>
2021-12-18 01:00:51 +0300

    add globalprefix and globalpips

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('BOOTSTRAP');
   try {
     const app = await NestFactory.create(AppModule);
     const PORT = process.env.PORT || 5000;
+
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(PORT, () =>
       logger.log(`Server started on ${PORT} port...`),
