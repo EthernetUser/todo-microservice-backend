e74a79f9bd479f977d79f3cb9e2784f516dedc4a
Alexander Melnikov <okman5757@gmail.com>
2021-12-18 01:04:44 +0300

    add jwtModule register in authmodule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    JwtModule.register({
+      secret: 'supasecret',
+    }),
+  ],
+  controllers: [AuthService],
 })
 export class AuthModule {}
